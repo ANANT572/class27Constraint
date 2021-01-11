a103b2ced229404a5a954cb08d75c8dd1e6dd974
ANANT572 <72691268+ANANT572@users.noreply.github.com>
2021-01-11 17:44:50 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,111 +1,79 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, pig1;
-var backgroundImg,bg="sprites/bg.png",platform;
-var loga;
-var gameState = "on sling";
-var score = 0;
+var box1, box2, box3,box4,box5,box6,box7,box8,box9,box10,box11,box12,box13,box14,box15,box16,box17,box18,box19,box20, ground;
+var ball, rope
 
-function preload() {
-    getTime();
-}
+function setup() {
+  createCanvas(3000, 800);
+  engine = Engine.create();
+  world = engine.world;
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+  ground = new Ground(600, 600, 1200, 20);
 
+  box1 = new Box(900, 100, 70, 70);
+  box2 = new Box(900, 100, 70, 70);
+  box3 = new Box(900, 100, 70, 70);
+  box4 = new Box(900, 100, 70, 70);
+  box5 = new Box(900, 100, 70, 70);
+  box6 = new Box(900, 100, 70, 70);
+  box7 = new Box(800, 100, 70, 70);
+  box8 = new Box(800, 100, 70, 70);
+  box9 = new Box(800, 100, 70, 70);
+  box10 = new Box(800, 100, 70, 70);
+  box11 = new Box(800, 100, 70, 70);
+  box12 = new Box(800, 100, 70, 70);
+  box13 = new Box(700, 100, 70, 70);
+  box14 = new Box(700, 100, 70, 70);
+  box15 = new Box(700, 100, 70, 70);
+  box16 = new Box(700, 100, 70, 70);
+  box17 = new Box(700, 100, 70, 70);
+  box18 = new Box(700, 100, 70, 70);
+  box19 = new Box(700, 100, 70, 70);
+  box20 = new Box(700, 100, 70, 70);
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
-
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-    loga = new Log(230,180,80,PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(200,50);
-    sling = new SlingShot(bird.body,{x:200,y:50});
-    
+  ball = new Ball(200, 200, 80, 80);
+  rope = new Rope(ball.body, { x: 500, y: 50 });
 
 }
 
-function draw(){
-    if(backgroundImg)
-    background(backgroundImg);
-    noStroke();
-    textSize(35);
-    fill("white");
-    text("score: "+score,width-300,50);
-    Engine.update(engine);
-    //console.log(box2.body.position.x);
-    //console.log(box2.body.position.y);
-    //console.log(box2.body.angle);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
+function draw() {
+  background(180);
+  Engine.update(engine);
+  ground.display();
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display()
+  box5.display()
+  box6.display()
+  box7.display()
+  box8.display()
+  box9.display()
+  box10.display()
+  box11.display()
+  box12.display()
+  box13.display()
+  box14.display()
+  box15.display()
+  box16.display()
+  box17.display()
+  box18.display()
+  box19.display()
+  box20.display()
+  
 
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
+  ball.display();
+  rope.display();
 
-    box5.display();
-    log4.display();
-    log5.display();
 
-    bird.display();
-    platform.display();
-    sling.display();
-    pig1.score();
-    pig3.score();
+
 }
-function mouseDragged(){
-    if(gameState !== "launched"){
-        Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
-}
-    }
-function mouseReleased(){
-    sling.fly(); 
-    gameState = "launched";
-    
-}
-function keyPressed(){
-    if(keyCode === 32){
-        //sling.attach(bird.body);
-    }
-}
- async function getTime(){
-  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
-   //console.log(response);
-   var responseJson = await response.json();
-   //console.log(responseJson);
-   var dateTime = responseJson.datetime;
-   //console.log(dateTime);
-   var hour = dateTime.slice(11,13);
-   console.log(hour);
-   if(hour>=06&&hour<=18){
-       bg = "sprites/bg.png";
-   }
-   else{
-       bg = "sprites/bg2.jpg";
-   }
-   backgroundImg = loadImage(bg);
+
+
+function mouseDragged() {
+  Matter.Body.setPosition(ball.body, { x: mouseX, y: mouseY });
 }
